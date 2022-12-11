09d7537bd931e066e5369bc664dfe31673cc9488
jc <root@sha3.dev>
2022-12-11 17:53:16 +0100

    refactoring

--- src/entities/tab.ts
+++ src/entities/tab.ts
@@ -31,11 +31,19 @@ export type TabViewPort = {
   height: number;
 };
 
+export type TabWaitAndScrollToBottomOptions = {
+  selectorToWait: string;
+  containerSelectorToScroll: string;
+  selectorToWaitAfterScroll: string;
+  waitTimeout?: number;
+};
+
 export type TabConfig = {
   url: string;
   viewport: TabViewPort;
   style?: string;
   headers?: Record<string, string>;
+  waitAndScrollOptions?: TabWaitAndScrollToBottomOptions;
 };
 
 /**
@@ -83,8 +91,15 @@ export default class Tab {
     return this.page.waitForFunction(wrappedFunction, { timeout });
   }
 
+  private async waitForSelector(selector: string, timeout: number) {
+    await this.waitForFunction(
+      (window: Window) => window.document.querySelector(selector),
+      timeout
+    );
+  }
+
   private async getScrollerOffsetHeight(selector: string) {
-    const offsetHeightValue = await this.exec((window: Window) => {
+    const offsetHeightValue = await this.execFunction((window: Window) => {
       const scroller = window.document.querySelector<HTMLDivElement>(selector);
       return scroller ? scroller.offsetHeight : null;
     });
@@ -92,7 +107,7 @@ export default class Tab {
   }
 
   private async getVerticalScroll(selector: string) {
-    const scrollValue = await this.exec((window: Window) => {
+    const scrollValue = await this.execFunction((window: Window) => {
       const verticalScroller =
         window.document.querySelector<HTMLDivElement>(selector);
       if (verticalScroller?.scrollHeight) {
@@ -112,6 +127,19 @@ export default class Tab {
     );
   }
 
+  private async moveScrollToBottom(selector: string) {
+    let scrollPerc = await this.getVerticalScroll(selector);
+    const scrollerHeight = await this.getScrollerOffsetHeight(selector);
+    while (scrollerHeight !== null && scrollPerc !== null && scrollPerc < 0.9) {
+      // eslint-disable-next-line no-await-in-loop
+      await this.moveVerticalScroll(selector, scrollerHeight);
+      // eslint-disable-next-line no-await-in-loop
+      scrollPerc = await this.getVerticalScroll(selector);
+      // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+
   /**
    * constructor
    */
@@ -125,14 +153,6 @@ export default class Tab {
   ) {}
 
   /**
-   * public: properties
-   */
-
-  public get Url() {
-    return this.page.url();
-  }
-
-  /**
    * public: methods
    */
 
@@ -141,6 +161,28 @@ export default class Tab {
     await this.setViewport(this.tabConfig.viewport);
     await this.setExtraHeaders(this.tabConfig.headers);
     await this.navigateToUrl(this.tabConfig.url);
+    if (this.tabConfig.waitAndScrollOptions?.containerSelectorToScroll) {
+      logger.debug(
+        `scrolling bottom to force lazy loading (container: ${this.tabConfig.waitAndScrollOptions.containerSelectorToScroll})`
+      );
+      if (this.tabConfig.waitAndScrollOptions.selectorToWait) {
+        await this.waitForSelector(
+          this.tabConfig.waitAndScrollOptions.selectorToWait,
+          this.tabConfig.waitAndScrollOptions.waitTimeout ||
+            CONFIG.DEFAULT_TIMEOUT_MS
+        );
+      }
+      await this.moveScrollToBottom(
+        this.tabConfig.waitAndScrollOptions.containerSelectorToScroll
+      );
+      if (this.tabConfig.waitAndScrollOptions.selectorToWaitAfterScroll) {
+        await this.waitForSelector(
+          this.tabConfig.waitAndScrollOptions.selectorToWaitAfterScroll,
+          this.tabConfig.waitAndScrollOptions.waitTimeout ||
+            CONFIG.DEFAULT_TIMEOUT_MS
+        );
+      }
+    }
   }
 
   public async close() {
@@ -152,25 +194,5 @@ export default class Tab {
     const elem = await this.page.addStyleTag({ content: style });
     const element = new Element(this.page, elem);
     return element;
-  }
-
-  public async moveScrollToBottom(selector: string) {
-    let scrollPerc = await this.getVerticalScroll(selector);
-    const scrollerHeight = await this.getScrollerOffsetHeight(selector);
-    while (scrollerHeight !== null && scrollPerc !== null && scrollPerc < 0.9) {
-      // eslint-disable-next-line no-await-in-loop
-      await this.moveVerticalScroll(selector, scrollerHeight);
-      // eslint-disable-next-line no-await-in-loop
-      scrollPerc = await this.getVerticalScroll(selector);
-      // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
-  }
-
-  public async waitForSelector(selector: string, timeout: number) {
-    await this.waitForFunction(
-      (window: Window) => window.document.querySelector(selector),
-      timeout
-    );
   }
 }
